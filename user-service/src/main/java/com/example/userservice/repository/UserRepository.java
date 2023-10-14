77612947b7651c9b3f698b471f09e9a872edf185
hyukjin <cdh5771@naver.com>
2023-10-14 23:40:03 +0900

    feat: JpaRepository로 상속 변경

--- user-service/src/main/java/com/example/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.userservice.repository;
 
 import com.example.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
 
 }
