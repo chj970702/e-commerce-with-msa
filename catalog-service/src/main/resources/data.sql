ed60b3c2cc3538eb6c17a9e69e3d322d0bb5676d
hyukjin <cdh5771@naver.com>
2023-10-16 00:12:46 +0900

    style: data.sql 설명 수정

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- sql 파일을 통해 어플리케이션 실행 시 자동으로 테이블에 레코드 3개 생성
+-- data.sql이라는 파일이름은 어플리케이션 실행 시 자동으로 sql 쿼리 수행
 
 insert into catalogs(product_id, product_name, stock, unit_price)
     values('CATALOG-001', 'Berlin', 100, 1500);
