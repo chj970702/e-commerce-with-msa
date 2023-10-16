6799de86c1590ff9fe1b4d464ce04c57f8baa9ed
hyukjin <cdh5771@naver.com>
2023-10-16 17:58:57 +0900

    chore: actuator로 config 적용 방법 변경

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -26,7 +26,11 @@ public class UserController {
 
     @GetMapping("/health-check")
     public String status() {
-        return String.format("It is working in user-service on PORT %s", env.getProperty("local.server.port"));
+        return String.format("It is working in user-service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("/welcome")
